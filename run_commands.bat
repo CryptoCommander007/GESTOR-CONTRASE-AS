d810a77450ce792fda4b238f181e8d699f704fae
CryptoCommander007 <rivantped@gmail.com>
2024-07-11 16:05:04 -0400

    GESTOR-CONTRASE-AS

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "GESTOR-CONTRASE-AS"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/GESTOR-CONTRASE-AS.git
+git push -u origin main
